3a73f012a2e1f4217acecbe05b86656474548deb
Luiz Felipe Vera Cruz <39517928840@info.local>
2021-11-17 10:57:30 -0300

    continuacao da finalizacao do backend e comecar a focar no frontend

---
+++ back-End/SpMedicalGroup/SpMedicalGroup/Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SpMedicalGroup.Domains;
+using SpMedicalGroup.Interfaces;
+using SpMedicalGroup.Repository;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SpMedicalGroup.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUsuarioRepository _UsuarioRepository { get; set; }
+
+        public LoginController()
+        {
+            _UsuarioRepository = new UsuarioRepository();
+        }
+        /// <summary>
+        /// Método de login
+        /// </summary>
+        /// <param name="login">objeto loginViewModel</param>
+        /// <returns>JWT</returns>
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuario UsuarioBuscado = _UsuarioRepository.Login(login.Email, login.Senha);
+
+                if (UsuarioBuscado == null)
+                {
+                    return BadRequest("Usuario ou senha inválidos");
+                }
+
+                var minhasClaims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, UsuarioBuscado.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, UsuarioBuscado.IdUsuario.ToString()),
+                    new Claim(ClaimTypes.Role, UsuarioBuscado.IdTipoUsuario.ToString()),
+
+                    new Claim("role", UsuarioBuscado.IdTipoUsuario.ToString())
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("spmg-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var meuToken = new JwtSecurityToken(
+
+                    issuer: "spmed.webAPI",
+                    audience: "spmed.webAPI",
+                    claims: minhasClaims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(meuToken)
+                });
+
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+    }
+}
---
+++ back-End/SpMedicalGroup/SpMedicalGroup/Controllers/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SpMedicalGroup.Controllers
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
